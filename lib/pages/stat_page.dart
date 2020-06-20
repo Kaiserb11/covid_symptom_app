3d18878402da650fff358d1459a9f3480c691d0e
Krishanu Bhattachajee <47850005+Kaiserb11@users.noreply.github.com>
2020-06-20 14:24:58 +0530

    Update stat_page.dart

--- lib/pages/stat_page.dart
+++ lib/pages/stat_page.dart
@@ -33,11 +33,11 @@ SliverToBoxAdapter _buildHeader(double screenHeight) {
           child: Stack(
             children: <Widget>[
               Container(
-                padding: EdgeInsets.fromLTRB(205,0,205,70),
+                padding: EdgeInsets.fromLTRB(205,0,205,75),
                 color: Colors.blueGrey[700],
                 child: Column(
                   children: <Widget>[
-                    SizedBox(height: screenHeight * 0.15),
+                    SizedBox(height: screenHeight * 0.155),
                   ],
                 ),
               ),
@@ -52,16 +52,207 @@ SliverToBoxAdapter _buildHeader(double screenHeight) {
                       fontWeight: FontWeight.w500)),
               ),
               Container(
-                padding: EdgeInsets.fromLTRB(16, 35, 0, 0),
+                padding: EdgeInsets.fromLTRB(16, 20, 16, 0),
                 margin: const EdgeInsets.symmetric(
-                  vertical: 70,
+                  vertical: 60,
                   horizontal: 14,
                 ),
-                height: screenHeight*0.30,
+                height: screenHeight*0.36,
                 decoration: BoxDecoration(
+                  boxShadow: [
+                    BoxShadow(
+                    color: Colors.blueGrey[700],
+                    blurRadius: 4.0,
+                    spreadRadius: 0.0,
+                    )
+                  ],
                   color: Colors.yellowAccent[700],
                   borderRadius: BorderRadius.circular(20),
                 ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                  Container(
+                    child: Text(
+                      'Assam',
+                      style: TextStyle(
+                        fontFamily: 'Monterrsat',
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.blueGrey[700],
+
+                      ),
+                    ),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Container(
+                        padding: EdgeInsets.only(top: 10),
+                        child: Text(
+                          'Confirmed',
+                          style: TextStyle(
+                            color: Colors.blueGrey[700],
+                            fontWeight: FontWeight.w700,
+                            fontSize: 14,
+                          ),
+                        ),
+                      ),
+                       Container(
+                        padding: EdgeInsets.only(top: 10),
+                        child: Text(
+                          'Recovered',
+                          style: TextStyle(
+                            color: Colors.blueGrey[700],
+                            fontWeight: FontWeight.w700,
+                            fontSize: 14,
+                          ),
+                        ),
+                      ),
+                       Container(
+                        padding: EdgeInsets.only(top: 10,),
+                        child: Text(
+                          'Deceased',
+                          style: TextStyle(
+                            color: Colors.blueGrey[700],
+                            fontWeight: FontWeight.w700,
+                            fontSize: 14,
+                          ),
+                        ),
+                      ),
+                  ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Container(
+                        padding: EdgeInsets.only(top: 10, left: 10,bottom: 10),
+                        child: Text(
+                          '4904',
+                          style: TextStyle(
+                            color: Colors.red[700],
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20,
+                          ),
+                        ),
+                      ),
+                       Container(
+                        padding: EdgeInsets.only(top: 10,bottom: 10),
+                        child: Text(
+                          '3039',
+                          style: TextStyle(
+                            color: Colors.green[700],
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20,
+                          ),
+                        ),
+                      ),
+                       Container(
+                        padding: EdgeInsets.only(top: 10, right: 30, bottom: 10),
+                        child: Text(
+                          '9',
+                          style: TextStyle(
+                            color: Colors.blueGrey[700],
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20,
+                          ),
+                        ),
+                      ),
+                  ],
+                  ),
+                  Divider(
+                    color: Colors.grey,
+                    ),
+                  Container(
+                    padding: EdgeInsets.only(top: 10),
+                    child: Text(
+                      'Across India',
+                      style: TextStyle(
+                        fontFamily: 'Monterrsat',
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.blueGrey[700],
+
+                      ),
+                    ),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Container(
+                        padding: EdgeInsets.only(top: 10),
+                        child: Text(
+                          'Confirmed',
+                          style: TextStyle(
+                            color: Colors.blueGrey[700],
+                            fontWeight: FontWeight.w700,
+                            fontSize: 14,
+                          ),
+                        ),
+                      ),
+                       Container(
+                        padding: EdgeInsets.only(top: 10),
+                        child: Text(
+                          'Recovered',
+                          style: TextStyle(
+                            color: Colors.blueGrey[700],
+                            fontWeight: FontWeight.w700,
+                            fontSize: 14,
+                          ),
+                        ),
+                      ),
+                       Container(
+                        padding: EdgeInsets.only(top: 10),
+                        child: Text(
+                          'Deceased',
+                          style: TextStyle(
+                            color: Colors.blueGrey[700],
+                            fontWeight: FontWeight.w700,
+                            fontSize: 14,
+                          ),
+                        ),
+                      ),
+                  ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Container(
+                        padding: EdgeInsets.only(top: 10, left: 10,bottom: 10),
+                        child: Text(
+                          '4904',
+                          style: TextStyle(
+                            color: Colors.red[700],
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20,
+                          ),
+                        ),
+                      ),
+                       Container(
+                        padding: EdgeInsets.only(top: 10,bottom: 10),
+                        child: Text(
+                          '3039',
+                          style: TextStyle(
+                            color: Colors.green[700],
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20,
+                          ),
+                        ),
+                      ),
+                       Container(
+                        padding: EdgeInsets.only(top: 10, right: 30, bottom: 10),
+                        child: Text(
+                          '9',
+                          style: TextStyle(
+                            color: Colors.blueGrey[700],
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20,
+                          ),
+                        ),
+                      ),
+                  ],
+                  ),
+                ],),
               ),
             ],
           ),
